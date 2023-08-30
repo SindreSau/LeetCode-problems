c4d8df7122c9e18af028aec1aad906d7c4e274b6
Sindre Sauarlia <101213882+SindreSau@users.noreply.github.com>
2023-08-30 20:27:28 +0200

    Completed 2451 - OddStringDifference

--- src/main/java/problems/OddStringDifference.java
+++ src/main/java/problems/OddStringDifference.java
@@ -1,18 +1,85 @@
 package problems;
 
-import java.util.Hashtable;
-import java.util.Map;
+import java.sql.Array;
+import java.util.*;
 
 /**
  * Solution to problem 2451. Odd String Difference
  * */
 public class OddStringDifference {
 
+    public static void main(String[] args) {
+        String[] words = {"adc", "wzy", "abc"};
+        String[] words2 = {"aaa","bob","ccc","ddd"};
 
+        System.out.println(oddString(words));
+        System.out.println(oddString(words2));
+    }
+
+    // Help from solutions
+    /*public static String oddString(String[] words) {
+        // List to store one differenceArray(e.g. [3, -1]) and one or more "words". We can then return the word that is
+        // stored with the key that has a list of length 1!
+        Map<List<Integer>, List<String>> map = new HashMap<>();
+
+        // Running through each word
+        for (int i = 0; i < words.length; i++) {
+            char[] letters = words[i].toCharArray();
+            List<Integer> list = new ArrayList<>();
+
+            // Running through each letter
+            for (int j = 0; j < letters.length - 1; j++) {
+                list.add(letters[j + 1] - letters[j]);
+            }
+
+            if(map.containsKey(list)) {
+                map.get(list).add(words[i]);
+            } else {
+                List<String> l = new ArrayList<>();
+                l.add(words[i]);
+                map.put(list, l);
+            }
+        }
+
+        for (List<Integer> key : map.keySet()) {
+            if (map.get(key).size() == 1)
+                return map.get(key).get(0);
+        }
+
+        return"";
+    }*/
+    //First try
+    public static String oddString(String[] words) {
+        // Map to store the difference-list with a list of the strings that has that particular difference
+        HashMap<List<Integer>, List<String>> map = new HashMap<>();
+
+
+        for (int i = 0; i < words.length; i++) {
+            char[] letters = words[i].toCharArray();
+            List<Integer> diff = new ArrayList<>();
+            for (int j = 0; j < letters.length - 1; j++) {
+                diff.add(letters[ j + 1] - letters[j]);
+            }
+
+            if (map.containsKey(diff)) {
+                map.get(diff).add(words[i]);
+            } else {
+                List<String> l = new ArrayList<>();
+                l.add(words[i]);
+                map.put(diff, l);
+            }
+        }
+
+        // Looping through map with map.keySet returning the key as a list of integers
+        for (List<Integer> key : map.keySet()) {
+            // Returning the 0'th element(meaning the single string) of the string-list stored in the key that has a list of length 1
+            if (map.get(key).size() == 1)
+                return map.get(key).get(0);
+        }
+
+        return "";
+    }
 }
-
-
-
 /*
 TASK:
 You are given an array of equal-length strings words. Assume that the length of each string is n.
