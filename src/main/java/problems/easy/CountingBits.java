5f0715a8e135a8caccff509b288b26694979a2e8
Sindre Sauarlia <101213882+SindreSau@users.noreply.github.com>
2023-09-01 10:19:26 +0200

    extra solution added to 338

--- src/main/java/problems/easy/CountingBits.java
+++ src/main/java/problems/easy/CountingBits.java
@@ -10,19 +10,46 @@ public class CountingBits {
 
     public static void main(String[] args) {
         CountingBits cb = new CountingBits();
-        int n = 5;
+        int n = 5_000;
 
+        long timeBefore = System.currentTimeMillis();
         int[] arr = cb.countBits(n);
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            System.out.print(arr[i] + ", ");
         }
+        long time = System.currentTimeMillis() - timeBefore;
+        System.out.println("\nTime in millis: " + time + "ms");
+
+        // Solution found on leetcode
+        long timeBefore2 = System.currentTimeMillis();
+        int[] arr2 = cb.countBits2(n);
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + ", ");
+        }
+        long time2 = System.currentTimeMillis() - timeBefore2;
+        System.out.println("\nTime in millis: " + time2 + "ms");
     }
 
     public int[] countBits(int n) {
         int[] ans = new int[n + 1];
 
         for (int i = 0; i < ans.length; i++) {
+            /*bitCount returns the number of one-bits in the two's complement binary
+            representation of the specified int value*/
             ans[i] = Integer.bitCount(i);
+        }
+
+        return ans;
+    }
+
+    //? Other solutions
+    // Solution found on leetcode, by avg_code
+    public int[]countBits2(int n) {
+        int[] ans = new int[n + 1];
+        ans[0] = 0;
+
+        for (int i = 1; i < ans.length; i++) {
+            ans[i] = ans[i / 2] + i % 2;
         }
 
         return ans;
